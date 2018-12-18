a192873330260768fce4dd20f361c7e0d4dc753b
Tarık <tarikyayla33@gmail.com>
2018-12-18 13:29:41 +0300

    Question solved

---
+++ question4.sql
@@ -0,0 +1,16 @@
+﻿SELECT COUNT(*)  AS DIFF_LOGIN FROM (
+	SELECT dbo.i_user_login_logs.UserId_userId
+	FROM dbo.i_user_login_logs 
+	GROUP BY UserId_userId 
+	HAVING COUNT(UserId_userId)>3
+) z;
+
+/* 
+
+	if you want to exclude admin you can add 
+
+	EXCEPT(SELECT i_users.userId 
+	FROM dbo.i_users 
+	WHERE username = 'Admin')
+
+*/
