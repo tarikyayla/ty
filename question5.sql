b0d6fa875ec1fd18256a90e6cf9f05407252d0b7
Tarık <tarikyayla33@gmail.com>
2018-12-18 13:29:45 +0300

    Question solved

---
+++ question5.sql
@@ -0,0 +1,6 @@
+﻿SELECT dbo.i_users.username,dbo.i_users.emailaddress FROM dbo.i_users WHERE userId = (
+	SELECT TOP 1 UserId_userId FROM dbo.i_user_login_logs 
+	WHERE UserId_userId != 1 
+	GROUP BY UserId_userId 
+	ORDER BY COUNT(login_date) desc 
+	)
